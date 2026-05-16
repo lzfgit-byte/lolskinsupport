07173b8a4c898e41a62f5aa8d9bf72d421decb60
18074 <proxypppp@qq.com>
2026-05-16 22:54:42 +0800

    int

--- src/view/components/lolskin-chose-skin.vue
+++ src/view/components/lolskin-chose-skin.vue
@@ -2,8 +2,10 @@
   <a-drawer
     v-model:open="choseDrawerOpen"
     placement="right"
-    width="80vw"
+    width="95vw"
     :header-style="{ display: 'none' }"
+    style="background-color: #1b2128"
+    root-class-name="custom-no-padding-drawer"
     destroy-on-close
   >
     <DoLolskinChoseSkin :key="heroId"></DoLolskinChoseSkin>
@@ -12,7 +14,16 @@
 <script setup lang="ts">
   import useGlobalState from '@/hooks/use-global-state';
   import DoLolskinChoseSkin from '@/view/components/do-lolskin-chose-skin.vue';
-  const { heroId, choseDrawerOpen } = useGlobalState();
+  const { heroId, choseDrawerOpen, skinId } = useGlobalState();
 </script>
 
-<style scoped lang="less"></style>
+<style>
+  .custom-no-padding-drawer .ant-drawer-body {
+    padding: 0 !important;
+  }
+</style>
+<style scoped lang="less">
+  .content-box {
+    background-color: #1b2128;
+  }
+</style>
